30a7197ab9fd09b2fe961df28c63c656dce02d34
implicitdef <implicitdef@users.noreply.github.com>
2018-10-04 18:10:39 +0200

    test of speech api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+
+let onvoiceschangedDoneAlready = false;
+
+function prepareSpeechInstance() {
+  console.log("building instance");
+  const instance = window.speechSynthesis;
+  return new Promise(resolve => {
+    // chrome
+    if (instance.onvoiceschanged !== undefined && !onvoiceschangedDoneAlready) {
+      instance.onvoiceschanged = () => resolve(instance);
+      onvoiceschangedDoneAlready = true;
+    } else {
+      // FF
+      resolve(instance);
+    }
+  });
+}
 
 class App extends Component {
+  saySomething = () => {
+    prepareSpeechInstance().then(speech => {
+      const voices = speech.getVoices();
+      console.log("got voices " + voices.length);
+      console.log(voices.filter(_ => _.lang.includes("fr")).slice(0, 10));
+      const utterance = new SpeechSynthesisUtterance(
+        "Ah, chère amie, vous étiez là ?"
+      );
+      utterance.pitch = 1;
+      utterance.rate = 1;
+      utterance.volume = 1;
+      voices.filter(_ => _.lang.includes("fr")).forEach(voice => {
+        console.log("Saying it with ", voice);
+        utterance.voice = voice;
+        utterance.lang = voice.lang;
+        speech.speak(utterance);
+      });
+    });
+  };
+
+  onVoicesReady = synth => {
+    console.log("reading voices");
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <button
+        onClick={() => {
+          //this.saySomething();
+          this.saySomething();
+        }}
+      >
+        do something
+      </button>
     );
   }
 }
